e0885a7641bb12408cef8ae4443fe63d9979f4ac
MusaTG <49027797+MusaTG@users.noreply.github.com>
2021-10-14 18:51:14 +0300

    koleksiyonlar, oop, listeler gerekli işlemleri eklendi.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("Musa");
+
+            Console.WriteLine(names.Length);
+
+            names.Add("Elyesa");
+
+            Console.WriteLine(names.Length);
+
+            foreach (var isim in names.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
