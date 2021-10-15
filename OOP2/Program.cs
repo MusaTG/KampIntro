d5033a0b326f91833db0ab83bb0875480e6f4c4e
MusaTG <49027797+MusaTG@users.noreply.github.com>
2021-10-15 16:47:37 +0300

    Miras, soyutlama ,interface işlemleri eklendi.

---
+++ OOP2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "1231";
+            musteri1.Adi = "Musa";
+            musteri1.Soyadi = "Görgeç";
+            musteri1.TcNo = "12345642222";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "1232";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "12345612";
+
+            //Gerçek Müşteri -- Tüzel Müşteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+        }
+    }
+}
